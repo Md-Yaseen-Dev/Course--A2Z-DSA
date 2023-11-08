16a97bab25039c7a7de4261e0950e5be98ab6811
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-08 23:07:56 +0530

    while loop is completed

---
+++ 01. Course-L1/07. while loop/demo.js
@@ -0,0 +1,21 @@
+// same as for loop. but here it will until the condition stops.
+
+var x =1 ;
+while(x<10){
+console.log(x);
+x++;
+}
+
+// do -while 
+
+//before checking condition do will run one time then it checks condition.
+
+var y =10;
+
+do{
+
+ console.log( y + " running");
+
+ y++;
+}
+while(y<=20)
