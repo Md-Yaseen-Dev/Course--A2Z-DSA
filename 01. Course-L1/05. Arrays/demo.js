a49a2cf7edfb98e2560259512ff22bf0474eb10a
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-08 22:55:05 +0530

    arrays completed

---
+++ 01. Course-L1/05. Arrays/demo.js
@@ -0,0 +1,10 @@
+//  Arrays are stored in a contigonous memory location 
+let arr = [1,"yaseen", null, true];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+arr[4] = 100;
+
+console.log(arr);
