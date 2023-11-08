8836f221008863b0dc6bbb93f7fc9dcfc5065b5a
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-08 22:59:43 +0530

    for loop completed

---
+++ 01. Course-L1/06. For loop/demo.js
@@ -0,0 +1,6 @@
+// if you want to print 100 times . it will take so much of time. so to save time and complexity. use for loop.
+
+for(i =1 ; i<=100; i++){
+
+    console.log( i + " hello world")
+}
