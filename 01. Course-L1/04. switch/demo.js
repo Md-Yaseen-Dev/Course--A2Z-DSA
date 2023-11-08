69aa9b7e726284389f5b0dfecf55c275af50d028
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-08 22:48:20 +0530

    switch case completed

---
+++ 01. Course-L1/04. switch/demo.js
@@ -0,0 +1,19 @@
+function Days_in_aweek(day){
+
+switch(day){
+
+    case 1: console.log("sunday"); break;
+    case 2: console.log("monday"); break;
+    case 3: console.log("tuesday"); break;
+    case 4: console.log("wednesday"); break;
+    case 5: console.log("thursday"); break;
+    case 6: console.log("friday"); break;
+    case 7: console.log("saturday"); break;
+    default :
+    console.log("invalid")
+}
+}
+Days_in_aweek(3);
+Days_in_aweek(13);
+Days_in_aweek(5);
+
