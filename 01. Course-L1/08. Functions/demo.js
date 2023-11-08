507b39371388c0ab6469e9d81ce4d752e1d26de0
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-08 23:16:01 +0530

    functions completed

---
+++ 01. Course-L1/08. Functions/demo.js
@@ -0,0 +1,23 @@
+// set of statements used to calcualte the task.
+
+
+//  callling function
+
+function fun(name){
+
+    console.log("my name is " + name);
+}
+
+fun("yaseen");
+fun("Roshan");
+
+
+// returning function
+
+function sum(a,b){
+
+    return a+b;
+}
+console.log(sum (5,10));
+console.log(sum (15,8));
+
