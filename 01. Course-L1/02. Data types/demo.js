6bb4f9137e5cdface6b792a537b509d38a842548
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-08 22:20:47 +0530

    data types completed

---
+++ 01. Course-L1/02. Data types/demo.js
@@ -0,0 +1,22 @@
+// Data types are the values that can be stored  in variables.
+
+//  By using var
+
+var num =10;
+
+var string = "hello folks";
+
+var boolean = true;
+
+var No_value = null;
+
+var un_defined;
+
+var arr= [1,2,3,4]
+
+console.log(num);
+console.log(string);
+console.log(boolean);
+console.log(No_value);
+console.log(un_defined);
+console.log(arr);
