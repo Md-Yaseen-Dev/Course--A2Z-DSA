a3ef616d332fd454fe49d57f1b9e4791507c0ae8
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-10 00:09:44 +0530

    pattern5 completed

--- 03. Pattern Questions/05. pattern.js
+++ 03. Pattern Questions/05. pattern.js
@@ -1,4 +1,4 @@
-function pattern4(n) {
+function pattern5(n) {
     let str = "";
     for (i = 1; i <=n; i++) {
       for (j = 0; j < n-i+1; j++) {
@@ -9,4 +9,19 @@ function pattern4(n) {
     console.log(str);
   }
   
-  pattern4(5);
+  pattern5(5);
+
+//   other method
+
+function pattern_5(n) {
+    let str = "";
+    for (i = 1; i <=n; i++) {
+      for (j = 5; j >= i; j--) {
+        str += "*";
+      }
+      str += "\n";
+    }
+    console.log(str);
+  }
+  
+  pattern_5(5);
