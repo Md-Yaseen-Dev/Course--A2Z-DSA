c5b06e064e65e42c725a5629181cf179c2595735
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-09 23:36:00 +0530

    pattern1 completed

---
+++ 03. Pattern Questions/01. Pattern.js
@@ -0,0 +1,23 @@
+// for(i=0;i <4; i++){
+//     for(j = 0 ; j< 4; j++){
+//      document.write("*");
+//     }
+//     document.write("<br>");
+// }
+
+
+
+
+function pattern1(){
+
+    for(i=0;i <4; i++){ 
+        let str = ""
+    for(j = 0 ; j< 4; j++){
+       str+="*"    
+    }
+    console.log(str);
+}
+
+}
+
+pattern1();
