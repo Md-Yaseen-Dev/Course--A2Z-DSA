f315053df17f7a2a2a62bcefbbe5f87a44fdae04
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-09 23:51:05 +0530

    pattern3 completed

---
+++ 03. Pattern Questions/03. pattern.js
@@ -0,0 +1,11 @@
+function pattern3(n) {
+  let str = "";
+  for (i = 1; i <= 5; i++) {
+    for (j = 1; j <= i; j++) {
+      str += j;
+    }
+    str += "\n";
+  }
+  console.log(str);
+}
+pattern3(5);
