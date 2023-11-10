2a5e8661f6e3e5574aca9145762db5d945de5478
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-11 00:00:33 +0530

    pattern17 completed

---
+++ 03. Pattern Questions/17. pattern.js
@@ -0,0 +1,39 @@
+function pattern17(n){
+
+    let str = ""
+    for(i=0; i< n ; i++){
+
+        // space
+
+        for(j=0; j < n-i-1; j++){
+
+            str+= " "
+        }
+
+        // name
+
+let count =0;
+        let breakpoint = (2*i+1)/2;
+        for (k=1 ; k <=2*i+1; k++){
+
+            str+=String.fromCharCode(65+count);;
+
+            if(k < breakpoint){
+                count++
+            }
+            else{
+                count--
+            }
+        }
+    
+        for(j=0; j < n-i-1; j++){
+
+            str+= " "
+        }
+str+="\n";
+    }
+
+    console.log(str)
+}
+
+pattern17(4)
