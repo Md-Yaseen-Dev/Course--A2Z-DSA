3b17d9e6888ebc9e52bd91367f8d935e866a37a4
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-13 15:57:17 +0530

    count digit added

---
+++ 04. Basic Maths-Euclidean Algorithm/01. Count Digits.js
@@ -0,0 +1,16 @@
+  function count_the_digit(n){
+
+    let count = 0;
+
+    while(n>0){
+        count++
+        
+         n = Math.log10(n);
+
+        // u Can do by using Math.trunc to remove fractional digits
+
+        // n = Math.trunc(n/10);
+    }
+console.log(n)
+  }
+  count_the_digit(805)
