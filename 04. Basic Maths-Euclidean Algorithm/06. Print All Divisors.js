b18e17446eab2f606653093b8da908998706ec82
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-14 22:42:01 +0530

    print all divisiors

--- 04. Basic Maths-Euclidean Algorithm/06. Print All Divisors.js
+++ 04. Basic Maths-Euclidean Algorithm/06. Print All Divisors.js
@@ -1,15 +1,39 @@
- function All_division(n){
+//  function All_division(n){
 
-    let divison = []
-    for(i=1; i<=n ; i++){
+//     let divison = []
+//     for(i=1; i<=n ; i++){
+
+//         if(n%i == 0){
+
+//             divison.push(i)
+//         }
+//     }
+//     console.log(divison);
+//  }
+
+//  All_division(36);
+//  All_division(45);
+
+
+
+
+function all_division(n){
+
+    let arr = []; 
+
+    for(i=1; i<= Math.sqrt(n) ; i++){
 
         if(n%i == 0){
 
-            divison.push(i)
+            arr.push(i);
+
+            if(n/i != i){
+                arr.push(n/i)
+            }
         }
     }
-    console.log(divison);
- }
 
- All_division(36);
- All_division(45);
+    console.log(arr.sort((a,b)=>{return a - b}))
+}
+
+all_division(36);
