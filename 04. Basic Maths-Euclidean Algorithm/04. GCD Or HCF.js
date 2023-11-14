24790190414f6f897c0f48e9909dc3ea304568a8
Mohammad Yaseen <ysn6040@gmail.com>
2023-11-14 23:44:17 +0530

    hcf completed

---
+++ 04. Basic Maths-Euclidean Algorithm/04. GCD Or HCF.js
@@ -0,0 +1,44 @@
+ function Gcd_1(n1,n2){
+
+    let gcd =1
+    for(i=1; i<= Math.min(n1,n2); i++){
+
+        if(n1% i == 0  && n2%i == 0){
+
+gcd = i        }
+    }
+    console.log(gcd)
+ }
+
+ Gcd_1(12,18);
+ Gcd_1(11,18);
+
+
+//  second method i.e equalateral algorithm
+
+
+function gcd_2(a,b){
+
+while(a> 0 && b>0){
+
+if(a>b){
+
+    a = a%b;
+}
+else{
+    b = b%a;
+
+}
+if(a==0){
+    console.log(b)
+}
+else{
+    console.log(a)
+
+}
+}
+}
+
+gcd_2(15,30);
+gcd_2(5,30);
+gcd_2(150,30);
