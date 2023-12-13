aa0029ad0a5ad57e69d77e0f21aaf81ffcd7d616
Mohammad Yaseen <ysn6040@gmail.com>
2023-12-13 10:02:39 +0530

    updated

--- 04. Basic Maths-Euclidean Algorithm/02. Reverse a Number.js
+++ 04. Basic Maths-Euclidean Algorithm/02. Reverse a Number.js
@@ -2,12 +2,13 @@
 function Reverse(n){
 
     let Reverse_num = 0;
-
     while(n>0){
 
         lastdigit = n%10;
-        Reverse_num = (Reverse_num*10) + lastdigit;
-        n = Math.trunc(n/10);
+        // console.log(lastdigit)
+        Reverse_num =  (Reverse_num*10) + lastdigit;
+        n =Math.trunc(n/10);
+        console.log(n);
     }
 console.log(Reverse_num);
 }
