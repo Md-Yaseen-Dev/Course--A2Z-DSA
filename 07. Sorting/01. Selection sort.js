ea2a94479d5c3b30b5ef963ae42304b9bf96beed
Mohammad Yaseen <ysn6040@gmail.com>
2023-12-13 22:37:13 +0530

    updated

--- 07. Sorting/01. Selection sort.js
+++ 07. Sorting/01. Selection sort.js
@@ -1,9 +1,15 @@
 function selection_sort(arr){
 
+
+    //here numbers are 13,46,30,24,52,9
+
+    // rows starts from 0 i.e 13
 for(i=0;i<= arr.length-2; i++){
 
     min = i ;
 
+
+    // it compares 13 with all the numbers . if there is number less than 13. it returns min.
     for(j=i ; j<= arr.length-1; j++){
 
         if(arr[j]< arr[min]){
@@ -16,6 +22,7 @@ for(i=0;i<= arr.length-2; i++){
     arr[min] = arr[i];
     arr[i] = temp ;
 
+    //here the small number get at first. and 13 replace at the position where small number index comes
 }
 return arr ; 
 }
