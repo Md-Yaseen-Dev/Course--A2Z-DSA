bfbd4897b8a0cebd9aa5eda19bf147a745978c79
Mohammad Yaseen <ysn6040@gmail.com>
2023-12-13 22:41:25 +0530

    updated

--- 07. Sorting/03. Insertion sort.js
+++ 07. Sorting/03. Insertion sort.js
@@ -1,8 +1,15 @@
 function insertion_sort(arr) {
+
+
   for (i = 0; i <= arr.length; i++) {
+
+    // from first index and second index they compare , if first value is larger and second value is smaller. they do swapping
+
+    // in index is three they check second index decrease 1 and check first index. and so on to the arr.length.
+
     j = i;
 
-    while (j > 0 && arr[j - 1] > arr[j]) {
+    while (j > 0 && arr[j - 1] > arr[j]) {   // 2 > 0 && arr[1] > arr[2]  (18 > 12)
 
         let temp = arr[j-1];
 
