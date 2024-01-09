52ab612bd542d01877646977b92e8075af08a27e
Mohammad Yaseen <ysn6040@gmail.com>
2024-01-10 01:27:38 +0530

    find the missing number completed

--- 08. Array/Easy/10. Find the misssing number .js
+++ 08. Array/Easy/10. Find the misssing number .js
@@ -27,20 +27,20 @@ let N = 5;
 function missingNumber(a, N) {
   for (i = 1; i <= N; i++) {
     let flag = 0;
-  
-  for (let j = 0; j < N - 1; j++) {
-    if (a[j] == i) {
-      //  i is present in the array
 
-      flag = 1;
-      break;
+    for (let j = 0; j < N - 1; j++) {
+      if (a[j] == i) {
+        //  i is present in the array
+
+        flag = 1;
+        break;
+      }
     }
-}
-  // check if the element is missing (i.e., flag === 0):
-  if (flag === 0) {
-    return i;
+    // check if the element is missing (i.e., flag === 0):
+    if (flag === 0) {
+      return i;
+    }
   }
-}
   // The following line will never execute.
   // It is just to avoid warnings.
   return -1;
@@ -48,18 +48,14 @@ function missingNumber(a, N) {
 
 console.log(missingNumber(a, N));
 
-
 // Time Complexity: O(N2), where N = size of the array+1.
 // Reason: In the worst case i.e. if the missing number is N itself, the outer loop will run for N times, and for every single number the inner loop will also run for approximately N times. So, the total time complexity will be O(N2).
 
 // Space Complexity: O(1)  as we are not using any extra space.
 
-
-
 // ------------------------------- Optimal approach------------
 
-// Approch : 
-
+// Approch :
 
 // The steps are as follows:
 
@@ -70,57 +66,45 @@ console.log(missingNumber(a, N));
 
 */
 
+function missingNumber1(a, N) {
+  const summation = (N * (N + 1)) / 2;
+  let sum = 0;
+  for (let i = 0; i < N - 1; i++) {
+    sum += a[i];
+  }
+  const missingNum = summation - sum;
 
-function missingNumber1(a, N){
-
-    const summation = (N*(N+1))/2 ;
-    let sum = 0;
-for(let i =0 ;i< N-1; i++){
-
-    sum +=a[i];
-}
-const missingNum = summation -sum;
-
-return missingNum
+  return missingNum;
 }
 
-console.log(missingNumber1(a,N));
-
+console.log(missingNumber1(a, N));
 
 // ime Complexity: O(N), where N = size of array+1.
 // Reason: Here, we need only 1 loop to get the sum of the array elements. The loop runs for approx. N times. So, the time complexity is O(N).
 
 // Space Complexity: O(1) as we are not using any extra space.
 
-
-
 // XOR  operator converts into binary -- if both values are 0 or 1 it returns 0. otherwise returns 1
 
-
 function missingNumber(a, N) {
-    let xor1 = 0;
-    let xor2 = 0;
-  
-    for (let i = 0; i < N - 1; i++) {
-      xor2 = xor2 ^ a[i];    //  0000  + 0001
+  let xor1 = 0;
+  let xor2 = 0;
 
-      xor1 = xor1 ^ (i + 1);   // 1) 0000  + 0001   2) -> 0001 + 0010  3)-> 0011 + 0011  -4)> 0000 + 0100
-      // XOR up to [1...N-1]
-      console.log("xo1 " + xor1);// XOR of array elements
+  for (let i = 0; i < N - 1; i++) {
+    xor2 = xor2 ^ a[i]; //  0000  + 0001
 
-    }
-    xor1 = xor1 ^ N; // XOR up to [1...N]
-  
-    return xor1 ^ xor2; // the missing number
+    xor1 = xor1 ^ (i + 1); // 1) 0000  + 0001   2) -> 0001 + 0010  3)-> 0011 + 0011  -4)> 0000 + 0100   --// XOR up to [1...N-1]
   }
-  
-  function main() {
-    const N = 5;
-    const a = [1, 2, 4, 5];
-    const ans = missingNumber(a, N);
-    console.log("The missing number is:", ans);
-  }
-  
-  main();
-  
-  
+  xor1 = xor1 ^ N; // XOR up to [1...N]
+
+  return xor1 ^ xor2; // the missing number
+}
+
+function main() {
+  const N = 5;
+  const a = [1, 2, 4, 5];
+  const ans = missingNumber(a, N);
+  console.log("The missing number is:", ans);
+}
+
+main();
