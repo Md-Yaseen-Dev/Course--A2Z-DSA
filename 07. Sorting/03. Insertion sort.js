98eb29f65ee5fb06b10a0578690bf644ebcc0e3c
Mohammad Yaseen <ysn6040@gmail.com>
2024-01-09 22:28:19 +0530

    insertion sort added

--- 07. Sorting/03. Insertion sort.js
+++ 07. Sorting/03. Insertion sort.js
@@ -11,7 +11,7 @@ function insertion_sort(arr) {
 
   for (i = 0; i <= arr.length; i++) {
 
-    // from first index and second index they compare , if first value is larger and second value is smaller. they do swapping
+    // from first index and second index they compare , if first value is larger and second value is smaller. they do swapping 
 
     // in index is three they check second index decrease 1 and check first index. and so on to the arr.length.
 
