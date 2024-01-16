784a7459f359e90ea3b80743fbef400476a3c942
Mohammad Yaseen <ysn6040@gmail.com>
2024-01-16 23:31:46 +0530

    print subarray with max subarray.js

---
+++ 08. Array/Medium/05. Print subarray with max subarray.js
@@ -0,0 +1,116 @@
+// ---------------------Print subarray with max subarray------------------
+// Problem atatement: Given an integer array arr, find the contiguous subarray containing at least one number which has the largest sum and prints the subarray.
+
+
+function maxSubarraySum(arr, n) {
+    let maxi = Number.MIN_SAFE_INTEGER; // maximum sum
+    for (let i = 0; i < n; i++) {
+        for (let j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+            let sum = 0;
+            //add all the elements of subarray:
+            for (let k = i; k <= j; k++) {
+                sum += arr[k];
+            }
+
+
+            maxi = Math.max(maxi, sum);
+        }
+    }
+
+    return maxi;
+}
+
+const arr = [ -2, 1, -3, 4, -1, 2, 1, -5, 4];
+const n = arr.length;
+const maxSum = maxSubarraySum(arr, n);
+console.log(`The maximum subarray sum is: ${maxSum}`);
+
+// Time complexity : O(N^3);
+
+// Space Complexity: O(1) as we are not using any extra space.
+
+
+
+// ----------------------Better approach ------------------------------
+
+
+// Approach:
+// The steps are as follows:
+
+// First, we will run a loop(say i) that will select every possible starting index of the subarray. The possible starting indices can vary from index 0 to index n-1(n = array size).
+// Inside the loop, we will run another loop(say j) that will signify the ending index as well as the current element of the subarray. For every subarray starting from index i, the possible ending index can vary from index i to n-1(n = size of the array).
+// Inside loop j, we will add the current element to the sum of the previous subarray i.e. sum = sum + arr[j]. Among all the sums the maximum one will be the answer.
+
+
+
+function maxSubarraySum1(arr, n) {
+    let maxi = Number.MIN_SAFE_INTEGER; // maximum sum
+
+    for (let i = 0; i < n; i++) {
+        let sum = 0;
+        for (let j = i; j < n; j++) {
+            // current subarray = arr[i.....j]
+
+            //add the current element arr[j]
+            // to the sum i.e. sum of arr[i...j-1]
+            sum += arr[j];
+
+            maxi = Math.max(maxi, sum); // getting the maximum
+        }
+    }
+
+    return maxi;
+}
+
+
+let maxSum1 = maxSubarraySum1(arr, n);
+console.log("The maximum subarray sum is: " + maxSum);
+
+
+// ----------------------------Optimal approach ----------------------------------------
+
+
+
+
+function maxSubarraySum3(arr, n) {
+    let maxi = Number.MIN_SAFE_INTEGER; // maximum sum
+    let sum = 0;
+
+    let start = 0;
+    let ansStart = -1, ansEnd = -1;
+    for (let i = 0; i < n; i++) {
+
+        if (sum == 0) start = i; // starting index
+
+        sum += arr[i];
+
+        if (sum > maxi) {
+            maxi = sum;
+
+            ansStart = start;
+            ansEnd = i;
+        }
+
+        // If sum < 0: discard the sum calculated
+        if (sum < 0) {
+            sum = 0;
+        }
+    }
+
+    //printing the subarray:
+    console.log("The subarray is: [");
+    for (let i = ansStart; i <= ansEnd; i++) {
+        console.log(arr[i] + " ");
+    }
+    console.log("]n");
+
+    // To consider the sum of the empty subarray
+    // uncomment the following check:
+
+    //if (maxi < 0) maxi = 0;
+
+    return maxi;
+}
+let maxSum2 = maxSubarraySum3(arr, n);
+console.log("The maximum subarray sum is: " + maxSum2);
