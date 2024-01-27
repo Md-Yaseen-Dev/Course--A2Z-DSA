d53974edc34ded618123f8d9e7d98d8bbddb10ee
Mohammad Yaseen <ysn6040@gmail.com>
2024-01-27 23:13:11 +0530

    binary search Recursive implementation

--- 09. Binary search/Js/01. Binary search to find x is sorted.js
+++ 09. Binary search/Js/01. Binary search to find x is sorted.js
@@ -126,3 +126,35 @@ So the overall time complexity is O(logN), where N = size of the given array.
 
 */
 
+// Recursive implementation:
+// Pre-requisite: Recursion section
+
+// Approach:
+// Assume, the recursive function will look like this: binarySearch(nums, low, high). It basically takes 3 parameters i.e. the array, the low pointer, and the high pointer. In each recursive call, we will change the value of low and high pointers to trim down the search space. Except for this, the rest of the steps will be the same.
+
+// The steps are as follows:
+
+// Step 1: Divide the search space into 2 halves:
+// In order to divide the search space, we need to find the middle point of it. So, we will take a ‘mid’ pointer and do the following:
+// mid = (low+high) // 2 ( ‘//’ refers to integer division)
+// Step 2: Compare the middle element with the target and trim down the search space:
+// In this step, we can observe 3 different cases:
+// If arr[mid] == target: We have found the target. From this step, we can return the index of the target, and the recursion will end.
+// If target > arr[mid]: This case signifies our target is located on the right half of the array. So, the next recursion call will be binarySearch(nums, mid+1, high).
+// If target < arr[mid]: This case signifies our target is located on the left half of the array. So, the next recursion call will be binarySearch(nums, low, mid-1).
+// Base case: The base case of the recursion will be low > high. If (low > high), the search space becomes invalid which means the target is not present in the array.
+// Note: For a better understanding, please watch the video at the bottom of the page.
+
+let nums = [3,4,6,7,8,9,11,16.17]
+function binarySearch1(nums, low, high, target) {
+    if (low > high) return -1; // Base case.
+
+    // Perform the steps:
+    let mid = Math.floor((low + high) / 2);
+    if (nums[mid] === target) return mid;
+    else if (target > nums[mid])
+        return binarySearch1(nums, mid + 1, high, target);
+    return binarySearch1(nums, low, mid - 1, target);
+}
+
+console.log(binarySearch1(nums,0,nums.length-1,6))
